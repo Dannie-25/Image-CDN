34980543a2bd1322641f25d6f5d776c6da0a95e8
Dannie-25 <drake.meav@gmail.com>
2024-02-27 10:38:49 -0600

    CloudFlare

--- App.js
+++ App.js
@@ -1,16 +1,15 @@
-import React, { useState, useEffect } from 'react';
-import { Button, Image, View, Platform } from 'react-native';
+import React, { useState } from 'react';
+import { Button, Image, View, Text, StyleSheet } from 'react-native';
 import * as ImagePicker from 'expo-image-picker';
 import { ref, uploadBytes } from 'firebase/storage';
 import { storage } from './firebase';
 import axios from 'axios';
 
-
 export default function ImagePickerExample() {
   const [image, setImage] = useState(null);
+  const [variants, setVariants] = useState([]);
 
   const pickImage = async () => {
-    // No permissions request is necessary for launching the image library
     let result = await ImagePicker.launchImageLibraryAsync({
       mediaTypes: ImagePicker.MediaTypeOptions.All,
       allowsEditing: true,
@@ -20,15 +19,13 @@ export default function ImagePickerExample() {
 
     console.log(result);
 
-    if (!result.canceled) {
+    if (!result.cancelled) {
       setImage(result.assets[0].uri);
     }
   };
 
   const onSubmit = async () => {
-
-    const imageRef = ref(storage, 'image.jpg')
-
+    const imageRef = ref(storage, 'image.jpg');
     const response = await fetch(image);
     const blob = await response.blob();
 
@@ -36,43 +33,71 @@ export default function ImagePickerExample() {
       console.log('Uploaded a blob or file!');
     });
 
-  }
+    await uploadImage();
+  };
 
-const [variants, setVariants] = useState([]);
-
-  async function uploadImage(){
+  async function uploadImage() {
     const formData = new FormData();
-    formData.append("file", image)
-  
-    axios.request({
-      url:"",
-      method:"POST",
-      data:formData,
-      headers: {
-        Authorization: "Bearer (aqui va la autenticacion del could)"
-      }
-    })
+    formData.append('file', image);
 
-    const {data: {result: {variants}}} = result;
+    const result = await axios.request({
+      url: 'https://api.cloudflare.com/client/v4/accounts/0ef61389d99814d54ebb4aa45b2b721e/images/v1/fb049658-453d-4264-48bd-506618ae0300',
+      method: 'GET',
+      headers: {
+        Authorization: 'Bearer V61ZfTsT2cv2bpHYDpAOQrTrHvSjkTLFgTgwEhbE',
+      },
+    });
+
+    const { data: { result: { variants } } } = result;
     setVariants(variants);
   }
 
-  /*{
-    "result": {
-      "id": "",
-      "filename":"",
+  {
+    "result"; {
+      "id"; "fb049658-453d-4264-48bd-506618ae0300",
+      "filename";"alone.jpg",
+      "uploaded"; "",
+      "requireSignedURLs"; false,
+      "variants"; [
+        "https://imagedelivery.net/37VC2dLndCZbm_Rdd_3gog/fb049658-453d-4264-48bd-506618ae0300/articulo>",
+        "https://imagedelivery.net/37VC2dLndCZbm_Rdd_3gog/fb049658-453d-4264-48bd-506618ae0300/banner>",
+        "https://imagedelivery.net/37VC2dLndCZbm_Rdd_3gog/fb049658-453d-4264-48bd-506618ae0300/icon>",
+        "https://imagedelivery.net/37VC2dLndCZbm_Rdd_3gog/fb049658-453d-4264-48bd-506618ae0300/public>"
+      ]
     }
-  }*/
+    "success"; true,
+    "errors"; [],
+    "message"; []
+  }
 
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
       <Button title="Pick an image from camera roll" onPress={pickImage} />
       {image && <Image source={{ uri: image }} style={{ width: 200, height: 200 }} />}
-      <Button title="Submit" onPress={onSubmit} />
 
-      
+      {variants.length > 0 && (
+        <View style={styles.variantsContainer}>
+          <Text>Variantes:</Text>
+          {variants.map((/*variant,*/index) => (
+            //<Text key={index}>{variant}</Text>
+            <Image key={index} source={{ uri: /*variant,*/image }} style={styles.variantImage} />
+          ))}
+        </View>
+      )}
+      <Button title="Submit" onPress={onSubmit} />
     </View>
   );
-
-
 }
+
+const styles = StyleSheet.create({
+  variantsContainer: {
+    marginTop: 20,
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  variantImage: {
+    width: 100,
+    height: 100,
+    marginVertical: 10,
+  },
+});
